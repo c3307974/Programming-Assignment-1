2d62dd95178ace645ece1c2fbd62f36d020730fc
c3307974 <49465298+c3307974@users.noreply.github.com>
2019-04-25 01:43:35 +1000

    Update main.c

--- main.c
+++ main.c
@@ -71,3 +71,49 @@ void encription_rotation()
 	no params
 	no retrun value
 	limitations: input cipher can't be longer then MESSAGE_LEN, key input must be int
+*/
+decription_rotation_with_key()
+{
+	char message[MESSAGE_LEN]; // plain text message variable
+	char cipher[MESSAGE_LEN];// cipher text message variable
+	int tmpVal; // help variable for calculations
+	int key; // variable that holds key, rotation value
+	int i; // loop variable for indexing
+
+	fgets(message, MESSAGE_LEN, stdin); // flush stdin, read left \n sign
+
+	printf("Enter the cipher text (UPPERCASE): ");
+	fgets(cipher, MESSAGE_LEN, stdin); // read line of text - cipher
+
+	printf("Enter key - rotation amount [0..25]: ");
+	scanf("%d", &key); // read shift value
+
+	for (i = 0; i < strlen(cipher); i++) // for each letter in cipher
+	{
+		if ((cipher[i] >= a_ASCII) && (cipher[i] <= z_ASCII)) // if letter is lowercase
+		{
+			cipher[i] = cipher[i] - 32; // translate it to UPPERCASE
+		}
+
+		if ((cipher[i] >= A_ASCII) && (cipher[i] <= Z_ASCII)) // if it is a letter, exclude white spaces, numbers etc..
+		{
+			tmpVal = cipher[i] - key; // shift letter left
+
+			if (tmpVal > Z_ASCII) // if letter is beyond 'Z'
+				message[i] = tmpVal - ALPHABET_LEN; // substract from begining
+			else if (tmpVal < A_ASCII)
+				message[i] = tmpVal + ALPHABET_LEN; // if letter is before 'A'
+			else
+				message[i] = tmpVal; // substract from begining
+
+		}
+		else
+		{
+			message[i] = cipher[i]; // if character is not letter just copy
+		}
+	}
+
+	message[i] = '\0'; // add null terminator at the and to make valid string
+	printf("Message text: %s\n", message); // print message plain text
+
+}
